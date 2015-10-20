428318dec8ad39bf574a6776dbe229b23467d294
joseacuna <joseacunaacuna@gmail.com>
2015-10-20 19:10:27 -0300

    agrego bootstrap, ademas de elaborar un form para las reservas

---
+++ app/controllers/Maqueta.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+
+/**
+ * Created by slenderman on 18-10-15.
+ */
+public class Maqueta extends Controller{
+
+    public static void index(){
+        render();
+    }
+    public static void menu() {render();}
+    public static void header(){render();}
+    public static void formReserva(){render();}
+}
